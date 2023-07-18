a87d1031eb662f7f9533e1293e2d56e8d79a87d8
Nikita Krasnov <nikitosikrasnov@gmail.com>
2023-07-18 19:05:53 +0300

    Limit Offset Pagination is added

--- RecipeAPI/settings.py
+++ RecipeAPI/settings.py
@@ -122,7 +122,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.'
-                                'PageNumberPagination',
+                                'LimitOffsetPagination',
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
